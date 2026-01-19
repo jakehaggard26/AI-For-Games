ef688d21ad650be16293f6072f7ad87628f82bf2
Jake <jhaggard26@gmail.com>
2026-01-19 11:34:32 -0500

    Overrided runSteeringArrival
    Can now take target as a Vector3 for an argument

--- Assets/Scripts/Movement/SteeringBehaviors.cs
+++ Assets/Scripts/Movement/SteeringBehaviors.cs
@@ -120,6 +120,57 @@ public class SteeringBehaviors
         return output;
     }
 
+        public static SteeringOutput runSteeringArrival(GameObject agent, Vector3 target)
+    {
+        SteeringOutput output = new SteeringOutput();
+
+        float targetSpeed = 0f;
+
+        // Get the direction to the target
+        output.Linear = target - agent.transform.position;
+        float distance = output.Linear.magnitude;
+
+        // Check if we are within the radius of satisfaction
+        if (distance < agent.GetComponent<AgentController>().radiusOfSatisfaction)
+        {
+            output.Linear = Vector3.zero;
+            return output;
+        }
+
+        // Check if we are within the slow radius
+        if(distance > agent.GetComponent<AgentController>().slowRadius)
+        {
+            targetSpeed = agent.GetComponent<AgentController>().maxSpeed;
+        }
+        else
+        {
+            // Calculate a scaled speed
+            targetSpeed = agent.GetComponent<AgentController>().maxSpeed * distance / agent.GetComponent<AgentController>().slowRadius;
+        }
+
+        // Target velocity combines speed and direction
+        Vector3 targetVelocity = output.Linear;
+        targetVelocity.Normalize();
+        targetVelocity *= targetSpeed;
+
+        // Acceleration tries to get to the target velocity
+        output.Linear = targetVelocity - agent.GetComponent<Rigidbody>().linearVelocity;
+        output.Linear /= agent.GetComponent<AgentController>().timeToTarget;
+
+        // Check if the acceleration is too fast
+        if (output.Linear.magnitude > agent.GetComponent<AgentController>().maxAcceleration)
+        {
+            output.Linear.Normalize();
+            output.Linear *= agent.GetComponent<AgentController>().maxAcceleration;
+        }
+
+        output.Angular = Vector3.zero;
+
+        Debug.DrawLine(agent.transform.position, agent.transform.position + output.Linear, Color.red);
+
+        return output;
+    }
+
     public static SteeringOutput runSteeringAlign(GameObject agent, GameObject target)
     {
         SteeringOutput output = new SteeringOutput();
@@ -243,8 +294,9 @@ public class SteeringBehaviors
         // Prep target for Seek
         output.Linear = target.transform.position + (target.GetComponent<Rigidbody>().linearVelocity * prediction);
 
-        // Delegate to Seek
+        // Delegate to Seek or Arrival
         output = runSteeringSeek(agent, output.Linear);
+        // output = runSteeringArrival(agent, output.Linear);
 
         return output;
     }
